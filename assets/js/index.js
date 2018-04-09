da02e65da79215a29f7dac0f2774a28ded93db51
MaraRODRIGUES <mara.rsousa@outlook.com>
2018-04-09 17:01:52 -0300

    Primeiro commit projeto2

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+// let sim = document.getElementById("yes");
+// yes.onclick = link;
+
+// let html = function(){
+//     location.href="http://google.com.br"
+//     console.log("ok");
+// };
+
+// document.querySelector('btn-no').onclick = function() {
+//     window.location.href="pergunta-02";
+// }
+
+
+
+// yes.addEventListener("click", html ());
+
+function btn(){
+    window.location.replaced("pergunta-03.html");
+}

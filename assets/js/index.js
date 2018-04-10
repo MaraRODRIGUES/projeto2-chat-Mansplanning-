46cf83e8a5f80087d3ecdeb5898126ea0614169d
MaraRODRIGUES <mara.rsousa@outlook.com>
2018-04-10 17:06:00 -0300

    Segundo commit Projeto2

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,14 +6,47 @@
 //     console.log("ok");
 // };
 
-// document.querySelector('btn-no').onclick = function() {
-//     window.location.href="pergunta-02";
-// }
+//
 
 
 
 // yes.addEventListener("click", html ());
 
-function btn(){
-    window.location.replaced("pergunta-03.html");
+var acertos = 0;
+var erros=0;
+var total = 10;
+var p = 0;
+var r = 0;
+
+//Criando um array para armazenar as imagens
+let bancoDeImgs = [
+    "01-sim.png", "02-sim.png", "03-sim.png", "04-sim.png", "05-sim.png", "06-sim.png", "07-sim.png", "08-sim.png", "09-sim.png", "10-sim.png"
+];
+
+let indiceBancoImg=0; //contador começando com 0;
+
+let i = 0;
+document.getElementById("imagem").src= "./assets/img/" + bancoDeImgs[i];
+//buscando o id no html/buscando a imagem no diretório/ ponto concatena método/ 
+
+function onclick(){
+    i++
+    document.querySelector("#imagem").src = "./assets/img/" + imagem[i];
 }
+
+document.querySelectorAll(".action").forEach(function(current, idx, callback)) {
+    current.addEventListener("click", onclick);
+} //pega o contador e o adiciona na lista de eventos ao click.
+
+
+
+
+
+
+
+
+//Armazenando os clicks
+
+
+
+
